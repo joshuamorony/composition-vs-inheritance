8a274c07819fa2e887b8fb823e4df9b3d4e2a8b2
Joshua Morony <joshua.morony@gmail.com>
2022-01-13 10:01:21 +1030

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'composition-vs-inheritance',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
