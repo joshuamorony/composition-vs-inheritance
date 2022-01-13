cb1428cfe270fe6be5b81692228f4133e515f433
Joshua Morony <joshua.morony@gmail.com>
2022-01-13 21:48:01 +1030

    make injectable

--- src/app/shared/page-base/list-page/list-page.service.ts
+++ src/app/shared/page-base/list-page/list-page.service.ts
@@ -1,6 +1,8 @@
+import { Injectable } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { debounceTime, filter } from 'rxjs/operators';
 
+@Injectable()
 export class ListPageService {
   searchControl: FormControl = new FormControl('');
   listSearch$ = this.searchControl.valueChanges.pipe(
